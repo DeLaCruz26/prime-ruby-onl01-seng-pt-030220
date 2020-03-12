02ded3a71893bf12d064d48892fd58f5eeb9c137
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-12 23:34:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
 def prime?(num)
-  (2..Math.sqrt(num)).each { |i| return false if num % i == 0}
-        true
-        
+  (2..Math.sqrt(num)).each { |i|
+    if num % i == 0 && i < num
+      return false
     end
+    }
+    true
+    end
+end
