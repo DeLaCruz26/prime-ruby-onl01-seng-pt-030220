f25c7f6da4d366481dd334de1496d2abdfbb5d79
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-12 23:41:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,8 @@ def prime?(num)
   if num < 0 or num == 0 or num == 2
     return false
   else
-    (2..num-1).to_a.all? do |
-
-
-
+    (2..num-1).to_a.all? do |int|
+      num % int != 0
+    end
   end
 end
